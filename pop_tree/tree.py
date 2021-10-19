faf5d03877c7f816738660a3621e06538ed17259
Acheul <72500819+acheul@users.noreply.github.com>
2021-10-20 01:49:07 +0900

    Update tree.py

--- pop_tree/tree.py
+++ pop_tree/tree.py
@@ -13,6 +13,8 @@ The dataframe assigned must follow a proper format to be organized.
 import pandas as pd
 dft = pd.read_csv("testtable.csv")
 
+A = 2
+
 def Tree(df=dft, format='svg', graph_name='OnceUponATimeInChosun',
          graph_attr={'pad':'1.4, 1', 'splines':'spline', 'nodesep':'.2', 'ranksep':'1.2', 'ratio':'auto', 'fontname':"NanumSquareRound", 'fontsize':"14pt"},
          node_attr={'fontname':"NanumSquareRound", 'fontsize':"14pt"}, edge_attr={'fontname':"NanumSquareRound", 'fontsize':"14pt"},
