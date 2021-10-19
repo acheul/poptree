f222af11258db32ce23f9c76af2db85831e9846e
Acheul <72500819+acheul@users.noreply.github.com>
2021-10-20 03:53:52 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'pop_tree',
-    version = '0.0.5',
+    version = '0.0.6',
     description = 'chosun Poppop Pop .. Tree',
     url = "https://github.com/acheul/tree.git",
     author = 'acheul',
