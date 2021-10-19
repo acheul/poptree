bc67b0edd533e384bbefa46f621d50ddb53ef695
Acheul <72500819+acheul@users.noreply.github.com>
2021-10-20 02:07:04 +0900

    Update tree.py

--- pop_tree/tree.py
+++ pop_tree/tree.py
@@ -10,11 +10,6 @@ If you do not assign a different dataframe, the function will use a default data
 The dataframe assigned must follow a proper format to be organized. 
 '''
 
-def init():
-  import pandas as pd
-  global dft
-  dft = pd.read_csv("testtable.csv")
-
 def Tree(df=dft, format='svg', graph_name='OnceUponATimeInChosun',
          graph_attr={'pad':'1.4, 1', 'splines':'spline', 'nodesep':'.2', 'ranksep':'1.2', 'ratio':'auto', 'fontname':"NanumSquareRound", 'fontsize':"14pt"},
          node_attr={'fontname':"NanumSquareRound", 'fontsize':"14pt"}, edge_attr={'fontname':"NanumSquareRound", 'fontsize':"14pt"},
