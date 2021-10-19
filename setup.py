b5d97afc08f4394f7ceab671c0fa597e057dcbc2
Acheul <72500819+acheul@users.noreply.github.com>
2021-10-20 01:31:17 +0900

    set up a setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'pop_tree',
+    version = '0.0.1',
+    description = 'chosun Poppop Pop .. Tree',
+    url = "https://github.com/acheul/tree.git",
+    author = 'acheul',
+    author_email = 'acheul',
+    license = "acheul",
+    packages = ['pop_tree'],
+    zip_safe = False,
+    install_requires = [
+    ]
+)
